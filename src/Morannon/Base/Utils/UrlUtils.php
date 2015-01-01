2eaf5b2f36ddd6c7a0a33bad2d38a50abcb19539
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2015-01-01 21:22:32 +0100

    add whatever mobile client

---
+++ src/Morannon/Base/Utils/UrlUtils.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Morannon\Base\Utils;
+
+final class UrlUtils
+{
+
+    /**
+     * Removes a trailing slash from an url string.
+     *
+     * @param string $url
+     * @return string
+     */
+    public function removeTrainingSlash($url)
+    {
+        if (null === $url || strlen($url) > 2) {
+            return $url;
+        }
+
+        return (substr($url, -1, 1) == '/')? substr($url, 0, -1) : $url;
+    }
+
+    /**
+     * Returns either the concatenated string or null on wrong params.
+     *
+     * @param string $baseUrl
+     * @param string $resource
+     * @return string|null Null on invalid {$baseUrl}
+     */
+    public function buildUrlString($baseUrl, $resource)
+    {
+        if (null === $baseUrl || '' == $baseUrl) {
+            return null;
+        }
+
+        if (null === $resource || '' == $resource) {
+            return $baseUrl;
+        }
+
+        $baseUrl = $this->removeTrainingSlash($baseUrl);
+        if (!substr($resource, 0, 1) == '/') {
+            $resource = '/' . $resource;
+        }
+
+        return $baseUrl . $resource;
+    }
+
+    /**
+     * Separates given string in key value parts.
+     *
+     * @param string $body
+     * @return array
+     */
+    public function parseNewLineSeparatedBody($body)
+    {
+        $data = array();
+
+        if (!$body) {
+            return $data;
+        }
+
+        $parts = explode(PHP_EOL, $body);
+        foreach ($parts as $str) {
+            list ($key, $value) = explode('=', $str);
+            $data[$key] = $value;
+        }
+
+        return $data;
+    }
+}
+ 
---
+++ src/Morannon/WhateverMobile/WhateverMobileGateway.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Morannon\WhateverMobile;
+
+use Guzzle\Http\Exception\RequestException;
+use Morannon\Base\Exception\DeliveryFailedException;
+use Morannon\Base\Gateway\AbstractHttpClientGateway;
+use Morannon\Base\Response\BaseSentResponse;
+use Morannon\Base\SMS\SMSInterface;
+
+class WhateverMobileGateway extends AbstractHttpClientGateway
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function sendSMS(SMSInterface $sms)
+    {
+        try {
+            $body = $this->httpClient->get(
+                $this->urlUtils->buildUrlString($this->getApiBaseUrl(), '/sendsms'),
+                null,
+                array(
+                    'query' => array(
+                        'user' => $this->getApiUser(),
+                        'password' => $this->getApiToken(),
+                        'from' => $sms->getFrom(),
+                        'to' => $sms->getTo(),
+                        'text' => mb_convert_encoding($sms->getText(), 'ISO-8859-15', mb_detect_encoding($sms->getText()))
+                    )
+                )
+            )->send();
+        } catch (RequestException $ex) {
+            throw new DeliveryFailedException($ex->getMessage(), 0, $ex);
+        }
+
+        $data = $this->urlUtils->parseNewLineSeparatedBody($body);
+        $sms->setMessageId($data['id']);
+
+        $response = new BaseSentResponse($sms, 1);
+        $response->setData($body);
+        $response->setMessageId($data['id']);
+
+        return $response;
+    }
+}
+ 
